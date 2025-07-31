c695d0b45193f50254c3f42252658863d3cf30a1
Frann7 <arielgomez316@hotmail.es>
2025-07-31 03:37:53 -0300

    prueba2

--- projects/main.cpp
+++ projects/main.cpp
@@ -2,6 +2,37 @@
 
 using namespace std;
 
+template<class T>
+class vector {
+    private:
+        T* data;
+        size_t size;
+        size_t capacity;
+    public:
+        vector(){};
+        void add(T datos);
+
+};
+
+template<class T>
+vector<T>::vector(){
+    this->size = 0;
+    this->capacity = 2; // Initial capacity
+    this->data = new T[this->capacity];
+}
+
+
+
+template<class T>
+void vector<T>::add(T datos) {
+    if (this->size == this->capacity) {
+        // Resize logic would go here
+    }
+
+    this->data[this->capacity++] = datos;
+}
+
+
 int main() {
     cout << "Hello, World!" << endl;
     return 0;
